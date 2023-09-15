3ef2538b737f2945fa20d97d05576438c165d156
Vlusky <vluskyhabib@gmail.com>
2023-09-15 15:08:14 +0700

    added mdx and updated astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://vlusky.github.io',
     base: '/',
-    integrations: [svelte()],
+    integrations: [svelte(), mdx()],
 });
