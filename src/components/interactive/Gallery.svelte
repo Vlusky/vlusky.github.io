8edf6d61b89df393a7f5835962a637a6f009ba63
Vlusky <vluskyhabib@gmail.com>
2024-01-05 22:31:06 +0700

    idiot forgot to remove the debug display

--- src/components/interactive/Gallery.svelte
+++ src/components/interactive/Gallery.svelte
@@ -44,8 +44,6 @@
 
 <h1 class="text-shadow">
   {title}
-  {loadedCurrent}
-  {loadedCount}
   <button class="material-symbols-rounded" on:click={toggle}>
     {isOpen ? "expand_more" : "chevron_right"}
   </button>
