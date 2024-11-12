2984646d8f0c053e16771ea5572df3cc55d3c609
Vlusky <vluskyhabib@gmail.com>
2024-11-12 12:34:09 +0700

    update to astro 4 (and svelte 5)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
+
 import svelte from '@astrojs/svelte';
+
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://vlusky.github.io',
-    base: '/',
-    integrations: [svelte(), mdx()],
+  integrations: [svelte(), mdx()]
 });
